4e19568e27236bba23705e3a0695b69aa44abfcb
Bayastan <bayastan.ishembaev@netigate.se>
2025-11-06 19:08:42 +0100

    resolve additional task related to lambok

--- src/lab4/CarLombok.java
+++ src/lab4/CarLombok.java
@@ -1,4 +1,14 @@
 package lab4;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarLombok {
+    private String model;
+    private String brand;
+    private int year;
+    private double price;
+    private String color;
+    private int quantity;
 }
